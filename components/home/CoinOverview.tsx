848e05f1d9279e9d87a382ed1a26dc1b5e83bbcc
adugbo aghogho <adugboaghogho@gmail.com>
2026-07-20 14:07:59 +0100

    update fetch parameters and auth headers for CoinGecko free tier

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -14,8 +14,7 @@ const CoinOverview = async () => {
       fetcher<OHLCData[]>('/coins/bitcoin/ohlc', {
         vs_currency: 'usd',
         days: 1,
-        interval: 'hourly',
-        precision: 'full',
+        // Removed 'interval' and 'precision' to comply with free API auto-granularity
       }),
     ]);
 
